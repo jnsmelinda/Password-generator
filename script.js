e67584a800d606231ea659cf4e4d30a0127bbddf
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-02 15:25:02 -0700

    getting length and adding default caracterset

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const charSet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "`~!@#$%^&*()_-+={}|[]\:\";\'<>?,./"];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    console.log(getLength());
+    console.log(chooseCharSets());
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
+
+}
+
+function getLength() {
+    var length = prompt("length between 8 and 128");
+    while (length < 8 || length > 128) {
+        alert("between 8 and 128");
+        length = prompt("length between 8 and 128");
+    }
+    return length;
+}
+
+function chooseCharSets() {
+
+}
+
+function generatePassword() {
+    var passWord = "";
+
+
+    console.log(charSet[0]);
 }
 
 // Add event listener to generate button
