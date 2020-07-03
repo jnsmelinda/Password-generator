6c02804912608bd77a2b57cd821b6b2d97f861c6
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-02 19:18:19 -0700

    allow to choose lower case too

--- script.js
+++ script.js
@@ -10,41 +10,50 @@ function writePassword() {
 }
 
 function generatePassword() {
-    var length = parseInt(getLength());
+    var length = getLength();
     var set = chooseCharSets();
 
     var result = "";
-    while (!validate(result, set) && result.length === 0) {
+    while (!validate(result, set)) {
         for (i = 0; i < length; i++) {
             result += set.charAt(Math.floor(Math.random() * set.length));
         }
     }
 
-    console.log(result);
-    console.log(validate(result, set));
+    // console.log(result.length);
+    // console.log(validate(result, set));
     return result;
 }
 
 function getLength() {
-    var length = prompt("length between 8 and 128");
+    var length = prompt("Choose the length of the password between 8 and 128");
     while (length < 8 || length > 128) {
         alert("between 8 and 128");
         length = prompt("length between 8 and 128");
     }
-    return length;
+    return parseInt(length);
 }
 
 function chooseCharSets() {
-    var selectedSet = charSet[0];
-    if (confirm("upper?") === true) {
-        selectedSet += charSet[1];
+    var selectedSet = "";
+    alert("Please select what caracter types would you like to include.");
+
+    while (selectedSet.length === 0) {
+        if (confirm("Lowercase caracters?") === true) {
+            selectedSet += charSet[0];
+        }
+        if (confirm("Uppercase caracters?") === true) {
+            selectedSet += charSet[1];
+        }
+        if (confirm("Numerical caracters?") === true) {
+            selectedSet += charSet[2];
+        }
+        if (confirm("Special caracters?") === true) {
+            selectedSet += charSet[3];
+        }
     }
-    if (confirm("num?") === true) {
-        selectedSet += charSet[2];
-    }
-    if (confirm("special?") === true) {
-        selectedSet += charSet[3];
-    }
+
+    // console.log(selectedSet);
 
     return selectedSet;
 }
