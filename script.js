5eee33e8c77ad92ee200f700782e85ddb1c79aa2
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-03 23:04:26 -0700

    refactor

--- script.js
+++ script.js
@@ -10,36 +10,82 @@ function writePassword() {
 }
 
 function generatePassword() {
-    var length = getLength();
-    var flags = getflags();
-    var selectedChars = getSelectedChars(flags);
+    var passwordLength = getPasswordLength();
+    var selectedCharSets = getSelectedCharSets();
+    var selectedChars = getSelectedChars(selectedCharSets);
 
-    var password = generate(length, selectedChars);
+    var password = generate(passwordLength, selectedChars);
 
-    while (!isValidPassword(password, flags)) {
-        password = generate(length, selectedChars);
+    while (!isValidPassword(password, selectedCharSets)) {
+        password = generate(passwordLength, selectedChars);
     }
 
     return password;
 }
 
-function isValidPassword(pass, inputFlags) {
-    var flags = new Array(charSets.length);
-
-    for (var i = 0; i < charSets.length; i++) {
-        flags[i] = containsAny(pass, charSets[i]);
+function getPasswordLength() {
+    var length = prompt("Choose the length of the password between 8 and 128");
+    while (length < 8 || length > 128) {
+        alert("between 8 and 128");
+        length = prompt("length between 8 and 128");
     }
+    return parseInt(length);
+}
 
-    for (var i = 0; i < flags.length; i++) {
-        if (flags[i] !== inputFlags[i]) {
-            return false;
+function getSelectedCharSets() {
+    var flags = new Array(charSets.length).fill(false);
+      alert("Please select what caracter sets would you like to include.");
+
+      while (!flags.find(element => element === true)) {
+          if (confirm("Lowercase caracters?") === true) {
+              flags[0] = true;
+          }
+          if (confirm("Uppercase caracters?") === true) {
+              flags[1] = true;
+          }
+          if (confirm("Numerical caracters?") === true) {
+              flags[2] = true;
+          }
+          if (confirm("Special caracters?") === true) {
+              flags[3] = true;
+          }
+      }
+
+      return flags;
+}
+
+function getSelectedChars(selectedCharSets) {
+    var selectedChars = "";
+    for (var i = 0; i < selectedCharSets.length; i++) {
+        if (selectedCharSets[i] === true) {
+            selectedChars += charSets[i];
         }
     }
 
-    return true;
+    return selectedChars;
 }
 
-function containsAny(value, setOfCharacters) {
+function generate(length, selectedChars) {
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        password += selectedChars.charAt(Math.floor(Math.random() * selectedChars.length));
+    }
+    console.log(`password: ${password}`);
+
+    return password;
+}
+
+function isValidPassword(pass, flagsFromInput) {
+    var flagsFromPassword = new Array(charSets.length);
+
+    for (var i = 0; i < charSets.length; i++) {
+        flagsFromPassword[i] = includesAny(pass, charSets[i]);
+    }
+
+    return compareFlags(flagsFromInput, flagsFromPassword);
+}
+
+function includesAny(value, setOfCharacters) {
     for (var i = 0; i < setOfCharacters.length; i++) {
         if (value.includes(setOfCharacters.charAt(i))) {
             return true;
@@ -49,57 +95,14 @@ function containsAny(value, setOfCharacters) {
     return false;
 }
 
-function getSelectedChars(flags) {
-    var selectedChars = "";
-    for (var i = 0; i < flags.length; i++) {
-        if (flags[i] === true) {
-            selectedChars += charSets[i];
+function compareFlags(flagsFromInput, flagsFromPassword) {
+    for (var i = 0; i < flagsFromInput.length; i++) {
+        if (flagsFromPassword[i] !== flagsFromInput[i]) {
+            return false;
         }
     }
 
-    return selectedChars;
-}
-
-function generate(length, selectedChars) {
-    var result = "";
-    for (var i = 0; i < length; i++) {
-        result += selectedChars.charAt(Math.floor(Math.random() * selectedChars.length));
-    }
-
-    // console.log("password: " + result);
-
-    return result;
-}
-
-function getLength() {
-    var length = prompt("Choose the length of the password between 8 and 128");
-    while (length < 8 || length > 128) {
-        alert("between 8 and 128");
-        length = prompt("length between 8 and 128");
-    }
-    return parseInt(length);
-}
-
-function getflags() {
-  var flags = new Array(charSets.length).fill(false);
-    alert("Please select what caracter sets would you like to include.");
-
-    while (!flags.find(element => element === true)) {
-        if (confirm("Lowercase caracters?") === true) {
-            flags[0] = true;
-        }
-        if (confirm("Uppercase caracters?") === true) {
-            flags[1] = true;
-        }
-        if (confirm("Numerical caracters?") === true) {
-            flags[2] = true;
-        }
-        if (confirm("Special caracters?") === true) {
-            flags[3] = true;
-        }
-    }
-
-    return flags;
+    return true;
 }
 
 // Add event listener to generate button
